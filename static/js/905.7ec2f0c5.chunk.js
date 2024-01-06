0fa185ec4e84ad6dde904716ad1617b6f3076b09
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2024-01-06 01:14:49 +0000

    Deploying to gh-pages from @ Sergey180989/goit-react-hw-08-phonebook@440a0678a38d47f149aefd6e33c9a934d88fbf21 🚀

---
+++ static/js/905.7ec2f0c5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[905],{5905:function(e,o,n){n.r(o),n.d(o,{default:function(){return h}});n(2791);var c=n(1981),t="HomePage_homePage__duLP8",a=n(3329),h=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsxs)("h1",{className:t,children:["Welcome to phonebook! ",(0,a.jsx)(c.q,{color:"blanchedalmond"})]})})}}}]);
+//# sourceMappingURL=905.7ec2f0c5.chunk.js.map
